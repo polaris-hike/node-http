3565b1970d689a6f713a339f69db8c10cd77e505
wuxuweilalala <2509165479@qq.com>
2020-04-27 21:56:38 +0800

    request 對对象

--- index.ts
+++ index.ts
@@ -1,10 +1,19 @@
 import * as http from 'http';
+import {IncomingMessage, ServerResponse} from 'http';
 
 const server = http.createServer();
 
-server.on('request',(request,response)=>{
-    console.log('有人请求了111');
+
+server.on('request',(request:IncomingMessage,response: ServerResponse)=>{
+    console.log('request.url');
+    console.log(request.url);
+    console.log('request.method');
+    console.log(request.method);
+    console.log('request.headers');
+    console.log(request.headers);
     response.end('hi')
 });
 
-server.listen(8888);
+server.listen(8888,()=>{
+    console.log(server.address())
+});

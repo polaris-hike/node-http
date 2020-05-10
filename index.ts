53e2add2c1697e8b05d7717ee19f96ddb4dc451a
wuxuweilalala <2509165479@qq.com>
2020-05-10 17:16:49 +0800

    根据url 不同请求不同文件

--- index.ts
+++ index.ts
@@ -1,29 +1,36 @@
 import * as http from 'http';
 import { IncomingMessage, ServerResponse } from 'http';
+import * as fs from 'fs';
+import * as p from 'path';
 
 const server = http.createServer();
-
+const publicDir = p.resolve(__dirname, 'public')
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    console.log('request.url');
-    console.log(request.url);
-    console.log('request.method');
-    console.log(request.method);
-    console.log('request.headers');
-    console.log(request.headers);
-    const arr = []
-    request.on('data', (chunk) => {
-        console.log('chunk')
-        console.log(chunk)
-        arr.push(chunk)
-    })
-    request.on('end', () => {
-        const body = Buffer.concat(arr).toString()
-        console.log('body')
-        console.log(body)
-        response.end('hi')
-    })
-
+    const { method, url, headers } = request;
+    switch (url) {
+        case '/index.html':
+            response.setHeader('Content-Type', 'test/html;charset=utf-8');
+            fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
+                if (error) throw error;
+                response.end(data.toString());
+            })
+            break;
+        case '/style.css':
+            response.setHeader('Content-Type', 'test/css;charset=utf-8');
+            fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
+                if (error) throw error;
+                response.end(data.toString());
+            })
+            break;
+        case '/main.ts':
+            response.setHeader('Content-Type', 'test/javascript;charset=utf-8');
+            fs.readFile(p.resolve(publicDir, 'main.ts'), (error, data) => {
+                if (error) throw error;
+                response.end(data.toString());
+            })
+            break;
+    }
 });
 
 server.listen(8888, () => {

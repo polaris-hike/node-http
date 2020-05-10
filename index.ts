4c4fd2369a56ec73d6d9018553063432d28b6f18
wuxuweilalala <2509165479@qq.com>
2020-05-10 17:47:39 +0800

    使用 url.parse 处理查询参数

--- index.ts
+++ index.ts
@@ -2,34 +2,39 @@ import * as http from 'http';
 import { IncomingMessage, ServerResponse } from 'http';
 import * as fs from 'fs';
 import * as p from 'path';
+import * as url from 'url';
 
 const server = http.createServer();
 const publicDir = p.resolve(__dirname, 'public')
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-    const { method, url, headers } = request;
-    switch (url) {
+    const { method, url: path, headers } = request;
+    const { pathname, search } = url.parse(path)
+    switch (pathname) {
         case '/index.html':
-            response.setHeader('Content-Type', 'test/html;charset=utf-8');
+            response.setHeader('Content-Type', 'text/html;charset=utf-8');
             fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
                 if (error) throw error;
                 response.end(data.toString());
             })
             break;
         case '/style.css':
-            response.setHeader('Content-Type', 'test/css;charset=utf-8');
+            response.setHeader('Content-Type', 'text/css;charset=utf-8');
             fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
                 if (error) throw error;
                 response.end(data.toString());
             })
             break;
-        case '/main.ts':
-            response.setHeader('Content-Type', 'test/javascript;charset=utf-8');
-            fs.readFile(p.resolve(publicDir, 'main.ts'), (error, data) => {
+        case '/main.js':
+            response.setHeader('Content-Type', 'text/javascript;charset=utf-8');
+            fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
                 if (error) throw error;
                 response.end(data.toString());
             })
             break;
+        default:
+            response.statusCode = 404;
+            response.end()
     }
 });
 

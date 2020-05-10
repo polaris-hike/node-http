46c0b49c53d0ebb708952bbcf7f7dfb8a13c564a
wuxuweilalala <2509165479@qq.com>
2020-05-10 11:02:23 +0800

    获取 post 请求体的内容

--- index.ts
+++ index.ts
@@ -1,19 +1,31 @@
 import * as http from 'http';
-import {IncomingMessage, ServerResponse} from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 
 const server = http.createServer();
 
 
-server.on('request',(request:IncomingMessage,response: ServerResponse)=>{
+server.on('request', (request: IncomingMessage, response: ServerResponse) => {
     console.log('request.url');
     console.log(request.url);
     console.log('request.method');
     console.log(request.method);
     console.log('request.headers');
     console.log(request.headers);
-    response.end('hi')
+    const arr = []
+    request.on('data', (chunk) => {
+        console.log('chunk')
+        console.log(chunk)
+        arr.push(chunk)
+    })
+    request.on('end', () => {
+        const body = Buffer.concat(arr).toString()
+        console.log('body')
+        console.log(body)
+        response.end('hi')
+    })
+
 });
 
-server.listen(8888,()=>{
+server.listen(8888, () => {
     console.log(server.address())
 });
